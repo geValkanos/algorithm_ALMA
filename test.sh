74d413e766cd8d121a6af99bac6a531e4d69cc0b
gvalkanos <gvalkanos@programize.com>
2018-11-02 21:09:35 +0200

    Add gitignore / test script

---
+++ test.sh
@@ -0,0 +1,42 @@
+# Correct input format
+# bash test.sh my_program input_Folder
+
+# Set the timeout in seconds
+TIMEOUT=1
+
+RED='\033[031m'
+GREEN='\033[032m'
+ORANGE='\033[033m'
+NC='\033[0m'
+
+# Check if the number of arguments is correct.
+if [ $# != 3 ]
+then
+    # First is the executabele.
+    # Second is the folder.
+    if [ -x $1 ] && [ -d $2 ]
+    then
+	input=$(find $2 | grep input*)
+	#Iterate the input files.
+	for testcase in $input
+	do
+	    correct_output=$(cat $2'output'${testcase##*input})
+	    runExecutable=$(timeout $TIMEOUT './'$1 < $testcase)
+	    if [ $? -eq 124 ]
+	    then
+		echo -e "${RED}TIMEOUT${NC}: ${ORANGE}${testcase}${NC} Execution took more than 1 second"
+	    else
+		if [ $runExecutable = $correct_output ]
+		then
+		    echo -e "[${GREEN}Correct${NC}]" $runExecutable 
+		else
+		    echo "[${RED}Wrong${NC}]" " ${ORANGE}${testcase}${NC} Correct answer:\n"$correct_output
+		fi
+	    fi
+	done
+    else
+	echo "Error: First argument must be executable, Second must be a folder"
+    fi
+else
+    echo "Error: Wrong number of arguments"
+fi
